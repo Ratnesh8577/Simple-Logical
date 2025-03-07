545ceee2d7301223e1a87b8096ba36b7081e78ee
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-07 22:50:20 +0530

    Add files via upload

---
+++ ComparinTwoString.py
@@ -0,0 +1,14 @@
+#Comparing Two Strings for ANAGRAMS
+str1="Listen"
+str2="Silent"
+
+str1=str1.replace(" "," ").upper()
+str2=str2.replace(" "," ").upper()
+
+if sorted(str1)==sorted(str2):
+    print("True")
+else:
+    print("False")
+
+
+    
