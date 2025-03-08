1b9877679faacfbfaa8e30ffc68abc7ebfef7e5a
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-08 22:36:19 +0530

    Add files via upload

---
+++ P2.(SNtoD.py
@@ -0,0 +1,7 @@
+#(2) Converting an String of Interger into Decimal
+import decimal #decimal ke jagah par par kuchh likh sakte hai
+string ="12345"
+print(decimal.Decimal(string))
+#Decimal ek function hai
+print(type(decimal.Decimal(string)))
+
