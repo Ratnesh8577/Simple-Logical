9bb1096ae4080d7072a12d17b5c093c83f2047c8
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-08 22:35:58 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,18 @@
+#(1) Converting an Integer into Decimls
+import decimal   
+#Importing the decimal module:
+# The decimal module provides support for fast and precise decimal floating-point arithmetic. It is especially useful for applications requiring exact decimal representation, such as financial calculations.
+#                    
+integer = 10
+#Converting the integer to a Decimal object:
+
+print(decimal.Decimal(integer))
+#Printing the type of the Decimal object:
+
+print(type(decimal.Decimal(integer)))
+
+
+
+
+
+
