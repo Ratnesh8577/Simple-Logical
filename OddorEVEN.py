403b47d795135d93abf0fd3cbc08ef24be2511e8
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-08 22:35:15 +0530

    Add files via upload

---
+++ OddorEVEN.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number: "))
+if num%2==0:
+    print(f"Enter number{num} is  even")
+else:
+    print(f"Enter number{num} is  odd")
+
+#Output:-Enter a number: 33
+#Enter number33 is  odd
+
