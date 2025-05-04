e1a37b101be3156bb043cc362c47546ed1fcb05e
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2025-05-04 22:52:16 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+def sum_of_array(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
+
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+print("Sum of array:", sum_of_array(numbers))
